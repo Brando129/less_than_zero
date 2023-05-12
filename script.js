cb3eb778bec0a1b7f025d2ea697f08e1ee7a01cc
Brandon Colangelo <brandoncancode@gmail.com>
2023-05-12 09:22:37 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+
+function less_than_or_egual(num){
+    if (num <= 0){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+console.log(less_than_or_egual(1))
